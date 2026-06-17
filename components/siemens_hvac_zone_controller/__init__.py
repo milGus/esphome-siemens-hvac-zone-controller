9d20a9c6d29ffc9a4c0690c62685d55b82928ad1
milGus <gusius.gus@milspec.com.au>
2026-06-17 14:20:41 +0930

    Update __init__.py

--- components/siemens_hvac_zone_controller/__init__.py
+++ components/siemens_hvac_zone_controller/__init__.py
@@ -5,7 +5,7 @@ CODEOWNERS = ["@milGus"]
 
 # Officially declare uart_id as an accepted configuration property
 CONFIG_SCHEMA = cv.Schema({
-    cv.Optional("uart_id", default="siemens_uart"): cv.use_id(uart.UARTComponent),
+    cv.Optional("uart_id", default="siemens_gus_uart"): cv.use_id(uart.UARTComponent),
 })
 
 async def to_code(config):

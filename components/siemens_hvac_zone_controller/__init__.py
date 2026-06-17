d7d3233259cbe1a96416ad08b437b152c27203d3
milGus <gusius.gus@milspec.com.au>
2026-06-17 14:18:26 +0930

    Update __init__.py

--- components/siemens_hvac_zone_controller/__init__.py
+++ components/siemens_hvac_zone_controller/__init__.py
@@ -1,9 +1,12 @@
 import esphome.config_validation as cv
+from esphome.components import uart
 
 CODEOWNERS = ["@milGus"]
 
-# This is a minimal valid configuration schema for a pure YAML component package
-CONFIG_SCHEMA = cv.Schema({})
+# Officially declare uart_id as an accepted configuration property
+CONFIG_SCHEMA = cv.Schema({
+    cv.Optional("uart_id", default="siemens_uart"): cv.use_id(uart.UARTComponent),
+})
 
 async def to_code(config):
     pass

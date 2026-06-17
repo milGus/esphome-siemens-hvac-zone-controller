3bf66249513435238bf10ea0085b37fa6c8a303a
milGus <gusius.gus@milspec.com.au>
2026-06-17 14:16:54 +0930

    Update __init__.py

--- components/siemens_hvac_zone_controller/__init__.py
+++ components/siemens_hvac_zone_controller/__init__.py
@@ -1,10 +1,8 @@
-import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import cv as cv_local
 
 CODEOWNERS = ["@milGus"]
 
-# This tells ESPHome to load the accompanying .yaml file from this directory
+# This is a minimal valid configuration schema for a pure YAML component package
 CONFIG_SCHEMA = cv.Schema({})
 
 async def to_code(config):
